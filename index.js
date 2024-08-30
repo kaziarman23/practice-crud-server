a495af7fb52950fe1439805143775e26f7ab6c82
Ethernal Knight <kaziarman@proton.me>
2024-08-30 12:43:20 +0600

    create & read api's are created in the server

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERPASSWORD}@knight-cluster.bypaq.mongodb.net/?retryWrites=true&w=majority&appName=knight-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +24,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const memberCollection = client.db("membersDB").collection("members");
+
+    // members api
+    app.get("/members", async (req, res) => {
+      const corsur = memberCollection.find();
+      const result = await corsur.toArray();
+      res.send(result);
+    });
+
+    app.post("/members", async (req, res) => {
+      const member = req.body;
+      const result = await memberCollection.insertOne(member);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,10 +50,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 // initial
 app.get("/", (req, res) => {
